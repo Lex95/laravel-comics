ce1fff7d1ce141a3c2f6a7f237aa9e88ae8185b7
Lex95 <andrea.lega1995@gmail.com>
2021-06-18 17:22:56 +0200

    aggiunta prima bozza header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,32 @@
 <header>
     <section>
-        <div class="container">
-            
+        <div class="container container-flex">
+            <div>Dc power visa®</div>
+            <div>
+                Additional dc sites
+                <i class="fa fa-caret-down" aria-hidden="true"></i>
+            </div>
         </div>
+    </section>
+    <section>
+        <div class="container container-flex">
+            <img src="..\..\public\images\dc-logo.png" alt="">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <input type="text">
+        </div>
+    </section>
+    <section>
+        <div id="hero"></div>
     </section>
 </header>
