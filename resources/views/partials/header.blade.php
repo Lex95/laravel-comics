c17209758ea8e1e478f052b024f16819ad444b65
Lex95 <andrea.lega1995@gmail.com>
2021-06-19 16:12:05 +0200

    completamento header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,7 @@
 <header>
-    <section>
+    <section id="hs1">
         <div class="container container-flex">
+            <div class="filler"></div>
             <div>Dc power visa®</div>
             <div>
                 Additional dc sites
@@ -8,23 +9,26 @@
             </div>
         </div>
     </section>
-    <section>
-        <div class="container container-flex">
-            <img src="..\..\public\images\dc-logo.png" alt="">
+    <section id="hs2">
+        <nav class="container container-flex">
+            <img src={{ asset("images/dc-logo.png")}} alt="">
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
             </ul>
-            <input type="text">
-        </div>
+            <div class="searchbar">
+                <input type="text" placeholder="Search">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+        </nav>
     </section>
     <section>
         <div id="hero"></div>
