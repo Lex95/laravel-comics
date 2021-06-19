0e01b414139f507b8da8d8573bc664a632f8738e
Lex95 <andrea.lega1995@gmail.com>
2021-06-19 16:55:29 +0200

    aggiunta prima parte footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,59 @@
 <footer>
-
+    <section id="fs1">
+        <div class="container container-flex">
+            <div class="left-side container-flex">
+                <div class="footer-links">
+                    <div class="col">
+                        <h3>Dc Comics</h3>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Tv</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <h3>Shop</h3>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3>DC</h3>
+                        <ul>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3>Sites</h3>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <p>All Site Cotnent TM and © 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.<br>
+                <a href="#">Cookies Settings</a></p>
+            </div>
+            <div class="filler"></div>
+            <div class="logo-container">
+                <img src={{ asset('images/dc-logo-bg.png') }} alt="">
+            </div>
+        </div>
+    </section>
 </footer>
